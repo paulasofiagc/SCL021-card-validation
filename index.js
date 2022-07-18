aa51e33d3f282b7eb1ed229d47274654c16a9aa8
paulasofiagc <paulasofiagc@gmail.com>
2022-07-18 15:11:26 -0400

    Updates

---
+++ index.js
@@ -0,0 +1,24 @@
+import validator from './validator.js';
+//Create the event to my "validateCard" bttn to generate the 'validator.js' instuction
+const validateBottom = document.getElementById("validateCard");
+//creando una función para traer el input N° de tarjeta y traer input Nombre.
+function validateCard() {
+    const numberCard = document.getElementById("numberCC").value;
+
+    const name= document.getElementById("Name").value;
+// Condiciones bajo la cual se maneja la función
+   //Si el largo total es menor o igual a = no permite continuar
+    if(numberCard.length <= 0) {
+      alert('Por favor ingrese un número de tarjeta.')  
+      //Si la funcion isValid es verdadera, se mostrará un alert aprobando.     
+    } else if(validator.isValid(numberCard) === true) {
+        const mask = validator.maskify(numberCard);
+        alert ( `${name} la tarjeta ${mask} es válida. ¡Tu reservación está lista! ✔.`) 
+        // si la función isValid es false, la tarjeta es inválida.
+     } else {
+        const mask = validator.maskify(numberCard);
+        alert (`${name} la tarjeta ${mask} es inválida.`)
+    }
+}
+//Al hacer click, hace correr la función.
+validateBottom.addEventListener("click", validateCard);
---
+++ validator.js
@@ -0,0 +1,32 @@
+const validator = {
+  isValid:(n) => {
+    //Matriz de dígitos, convertirlos a numeros dentro de la matriz
+  let digits = n.toString().split("").map(Number);
+  digits.reverse();
+    //Si los dígitos al ser divididos dan de resto 0. 
+  if(digits.length % 2 === 0) {
+    //Mapa de digitos,recreando matriz, si el digito en su resto es igual a 0 entonces lo multiplicamos
+    digits = digits.map((digit, idx)=> idx % 2 === 0 ? digit * 2 : digit);
+  } else {
+    digits = digits.map((digit, idx)=> idx % 2 === 1 ? digit * 2 : digit);
+    }
+    //Arreglar dígitos dobles para que se sumen. 
+    digits = digits.map(digit => digit > 9 ? digit - 9 : digit);
+    // suma de digitos
+    const sum = digits.reduce((acc, digit) => acc += digit, 0);
+    return sum % 10 === 0 ? true: false;
+   },
+   maskify: (n) => {
+    let maskifyNumber = n;
+    let finalNumber = '';
+    for (let i = 0; i < maskifyNumber.length; i++) {
+      if (i < maskifyNumber.length - 4) {
+        finalNumber = finalNumber + '#';
+      } else {
+        finalNumber = finalNumber + (maskifyNumber[i]); 
+      }     
+    }
+    return finalNumber;
+  }
+};
+export default validator ;
